a0a64fff756c8192b145a3a34bd81deadbee5775
Morgan Smith <msmith120304@gmail.com>
2025-10-22 23:03:10 -0600

    refactoring

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -28,8 +28,8 @@ public class Board {
     public Board() {
     	super();
     	roomMap = new HashMap<>();
-
-    	
+		targets = new HashSet<>();
+		visited = new HashSet<>();
     }
     
     
@@ -40,98 +40,88 @@ public class Board {
     
     public void initialize() {
     	//makes dummy 4x4 board for tests
-    	if(layoutCsv == null || setupTxt == null) {
-    		grid = new BoardCell[4][4];
-    		targets = new HashSet<>();
-	    	visited = new HashSet<>();
-    		for(int i = 0; i < 4; i++) {
-        		for(int j = 0; j < 4; j++) {
-        			grid[i][j] = new BoardCell(i, j);
-        		}
-        	}
-    	} else {
-	    	try {
-				loadSetupConfig();
-				loadLayoutConfig();
-			}
-			catch (BadConfigFormatException e) {
-				System.out.print(e);
-			}
-	    	
-	    	targets = new HashSet<>();
-	    	visited = new HashSet<>();
-	    	grid = new BoardCell[numRows][numCols];
-	    	
-	    	//setup board with cells
-	    	for(int i = 0; i < numRows; i++) {
-	    		String cells[] = rows.get(i).split(",");
-				for(int j = 0; j < numCols; j++) {
-	    			grid[i][j] = new BoardCell(i, j);
-					char roomChar = cells[j].charAt(0);
-					if (roomMap.containsKey(cells[j].charAt(0))) {
-						grid[i][j].setInRoom(true);
-						grid[i][j].setRoom(roomMap.get(roomChar));
-						
-						if (cells[j].length() == 2) {
-							char specialChar = cells[j].charAt(1);
-							if (roomMap.containsKey(specialChar)) {
-								grid[i][j].setIsSecretPassage(true);
-								grid[i][j].setSecretPassage(specialChar);
-							}
-							else {
-								switch (specialChar) {
-									case '*':
-										grid[i][j].setRoomCenter(true);
-										roomMap.get(roomChar).setCenterCell(grid[i][j]);
-										break;
-									case '#':
-										grid[i][j].setLabel(true);
-										roomMap.get(roomChar).setLabelCell(grid[i][j]);
-										break;
-									case '^':
-										grid[i][j].setDoorway(true);
-										grid[i][j].setDoorDirection(DoorDirection.UP);
-										break;
-									case '>':
-										grid[i][j].setDoorway(true);
-										grid[i][j].setDoorDirection(DoorDirection.RIGHT);
-										break;
-									case 'v':
-										grid[i][j].setDoorway(true);
-										grid[i][j].setDoorDirection(DoorDirection.DOWN);
-										break;
-									case '<':
-										grid[i][j].setDoorway(true);
-										grid[i][j].setDoorDirection(DoorDirection.LEFT);
-										break;
-								} 
-							}
+		try {
+			loadSetupConfig();
+			loadLayoutConfig();
+		}
+		catch (BadConfigFormatException e) {
+			System.out.print(e);
+		}
+
+		grid = new BoardCell[numRows][numCols];
+		
+		//setup board with cells
+		char roomChar;
+		char specialChar;
+		for(int i = 0; i < numRows; i++) {
+			String cells[] = rows.get(i).split(",");
+			for(int j = 0; j < numCols; j++) {
+				grid[i][j] = new BoardCell(i, j);
+				roomChar = cells[j].charAt(0);
+				if (roomMap.containsKey(roomChar)) {
+					grid[i][j].setInRoom(true);
+					grid[i][j].setRoom(roomMap.get(roomChar));
+					
+					if (cells[j].length() == 2) {
+						specialChar = cells[j].charAt(1);
+						if (roomMap.containsKey(specialChar)) {
+							grid[i][j].setIsSecretPassage(true);
+							grid[i][j].setSecretPassage(specialChar);
+						}
+						else {
+							switch (specialChar) {
+								case '*':
+									grid[i][j].setRoomCenter(true);
+									roomMap.get(roomChar).setCenterCell(grid[i][j]);
+									break;
+								case '#':
+									grid[i][j].setLabel(true);
+									roomMap.get(roomChar).setLabelCell(grid[i][j]);
+									break;
+								case '^':
+									grid[i][j].setDoorway(true);
+									grid[i][j].setDoorDirection(DoorDirection.UP);
+									break;
+								case '>':
+									grid[i][j].setDoorway(true);
+									grid[i][j].setDoorDirection(DoorDirection.RIGHT);
+									break;
+								case 'v':
+									grid[i][j].setDoorway(true);
+									grid[i][j].setDoorDirection(DoorDirection.DOWN);
+									break;
+								case '<':
+									grid[i][j].setDoorway(true);
+									grid[i][j].setDoorDirection(DoorDirection.LEFT);
+									break;
+							} 
 						}
 					}
-	    		}
-	    	}
-	    	
-	    	//find every adjacency of each cell, hint said to do it here
-	    	for(int i = 0; i < numRows; i++) {
-	    		for(int j = 0; j < numCols; j++) {
-	    			BoardCell cell = grid[i][j];
-	    			
-	    			if(i - 1 >= 0) {
-	    				cell.addAdjacency(grid[i-1][j]);
-	    			}
-	    			if(i + 1 < numRows) {
-	    				cell.addAdjacency(grid[i+1][j]);
-	    			}
-	    			if(j - 1 >= 0) {
-	    				cell.addAdjacency(grid[i][j-1]);
-	    			}
-	    			if(j + 1 < numCols) {
-	    				cell.addAdjacency(grid[i][j+1]);
-	    			}
-	    		}
-	    	}
-    	}
-    }
+				}
+			}
+		}
+		
+		//find every adjacency of each cell, hint said to do it here
+		for(int i = 0; i < numRows; i++) {
+			for(int j = 0; j < numCols; j++) {
+				BoardCell cell = grid[i][j];
+				
+				if(i-1 >= 0) {
+					cell.addAdjacency(grid[i-1][j]);
+				}
+				if(i+1 < numRows) {
+					cell.addAdjacency(grid[i+1][j]);
+				}
+				if(j-1 >= 0) {
+					cell.addAdjacency(grid[i][j-1]);
+				}
+				if(j+1 < numCols) {
+					cell.addAdjacency(grid[i][j+1]);
+				}
+			}
+		}
+	}
+
     
     
     public void setConfigFiles(String csv, String setup) {
@@ -144,19 +134,22 @@ public class Board {
 		try (BufferedReader reader = new BufferedReader(new FileReader(setupTxt))) {
 			String line;
 			while ((line = reader.readLine()) != null) {
-				String[] roomInfo = line.split(", ");
-				if (roomInfo[0].equals("Room") || roomInfo[0].equals("Space")) {
-					Room room = new Room();
-					room.setName(roomInfo[1]);
-					roomMap.put(roomInfo[2].charAt(0), room);
+				if (line.charAt(0) == '/') {
+					continue;
 				}
-
-				else if (line.charAt(0) == '/') {
-					// ignore
-				}
-
 				else {
-					throw new BadConfigFormatException();
+					String[] spaceInfo = line.split(", ");
+					String type = spaceInfo[0];
+					String name = spaceInfo[1];
+					char label = spaceInfo[2].charAt(0);
+					if (type.equals("Room") || type.equals("Space")) {
+						Room room = new Room();
+						room.setName(name);
+						roomMap.put(label, room);
+					}
+					else {
+						throw new BadConfigFormatException();
+					}
 				}
 			}
 		}
@@ -179,12 +172,15 @@ public class Board {
 			System.out.println(e);
 		}
 
-		String cells[] = rows.get(0).split(",");
-		int expectedNumCols = cells.length;
+		String regex = ",";
+		String firstRow[] = rows.get(0).split(regex);
+		String cells[];
+		int expectedNumCols = firstRow.length;
 		char roomChar;
-		boolean found = false;
+		char specialChar;
+		boolean isExtension = false;
 		for (String row : rows) {
-			cells = row.split(",");
+			cells = row.split(regex);
 			if (cells.length != expectedNumCols) {
 				throw new BadConfigFormatException();
 			}
@@ -194,14 +190,14 @@ public class Board {
 					throw new BadConfigFormatException();
 				}
 				else if (cell.length() == 2) {
-					char specialChar = cell.charAt(1);
+					specialChar = cell.charAt(1);
 					for (char c : validExtensions) {
 						if (specialChar == c) {
-							found = true;
+							isExtension = true;
 							break;
 						}
 					}
-					if (!found) {
+					if (!isExtension) {
 						throw new BadConfigFormatException();
 					}
 					else if (!roomMap.containsKey(specialChar)) {
@@ -219,7 +215,7 @@ public class Board {
 				}
 			}
 			numRows = rows.size();
-	        numCols = rows.get(0).split(",").length;
+	        numCols = expectedNumCols;
 		}
     }
     
