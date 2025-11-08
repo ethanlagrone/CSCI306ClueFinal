4b9781067ca99d776fb2bc5f9f8a8388709f3d4e
Morgan Smith <msmith120304@gmail.com>
2025-11-07 17:16:37 -0700

    Solution getters

--- src/clueGame/Solution.java
+++ src/clueGame/Solution.java
@@ -1,14 +1,26 @@
 package clueGame;
 
 public class Solution {
-	Card room;
-	Card person;
-	Card weapon;
+	private Card room;
+	private Card person;
+	private Card weapon;
 	
 	public Solution(Card room, Card person, Card weapon) {
 		super();
 		this.room = room;
 		this.person = person;
 		this.weapon = weapon;
+	}
+
+	public Card getRoom() {
+		return room;
+	}
+
+	public Card getPerson() {
+		return person;
+	}
+
+	public Card getWeapon() {
+		return weapon;
 	}
 }
