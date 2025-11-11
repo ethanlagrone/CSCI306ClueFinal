f20c97f31bd6626d58bc658a73cb2a447bc8e27e
Morgan Smith <msmith120304@gmail.com>
2025-11-11 00:06:22 -0700

    started createSuggestion but i am not doing ts tonight

--- src/clueGame/ComputerPlayer.java
+++ src/clueGame/ComputerPlayer.java
@@ -15,24 +15,31 @@ public class ComputerPlayer extends Player{
 	}
 	
 	public Solution createSuggestion(Room room) {
-		return null;
-		//stub
+		// room that the player is currently in (passed to this method)
+		Card roomCard = new Card(room.getName(), CardType.ROOM);
+		Card personCard = null;
+		Card weaponCard = null;
+		Card currentCard = null;
+		while (personCard == null || weaponCard == null) {
+			curre
+		}
+
 	}
 	
 	public BoardCell selectTarget(Set<BoardCell> targets) {
+		// get all rooms in the target (if any) that are unseen to the player
 		ArrayList<BoardCell> unseenRooms = new ArrayList<>();
 		for (BoardCell cell : targets) {
 			if (!cell.getRoom().getName().equals("Walkway") && !seen.contains(cell.getRoom().getName())) {
 				unseenRooms.add(cell);
 			}
 		}
-		for (BoardCell cell : unseenRooms) {
-			System.out.println(cell.getRoom().getName());
-		}
 
+		// if there are any unseen rooms in the target, pick a random one out of that list
 		if (unseenRooms.size() != 0) {
 			return unseenRooms.get((int)(Math.random() * unseenRooms.size()));
 		}
+		// if there are no unseen rooms in the target, pick a random target
 		else {
 			ArrayList<BoardCell> targetsList = new ArrayList<>(targets);
 			return targetsList.get((int)(Math.random() * targets.size()));
