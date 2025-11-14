b4cdbbd440ae194c1830fa875f732d1104ec5a32
ethanlagrone <ethan_lagrone@mines.edu>
2025-11-14 11:25:25 -0700

    Basic GameControlPanel created

--- src/clueGame/GameControlPanel.java
+++ src/clueGame/GameControlPanel.java
@@ -2,9 +2,73 @@ package clueGame;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 public class GameControlPanel extends JPanel {
 
+    private JTextField turnField;
+    private JTextField rollField;
+    private JTextField guessField;
+    private JTextField guessResultField;
+    
+    public GameControlPanel() {
+    	//constructor
+    	setLayout(new BorderLayout());
+    	
+    	//Big Panel two rows
+    	JPanel MainPanel = new JPanel();
+    	MainPanel.setLayout(new GridLayout(2,1));
+    	
+    	//Create the top panel in the big panel
+    	JPanel topMainPanel = new JPanel();
+    	JPanel top1Panel = new JPanel(new GridLayout(2,1));
+    	JPanel top2Panel = new JPanel();
+    	JButton nextPlayerButton = new JButton("NEXT!");
+    	JButton accusationButton = new JButton("Make Accusation");
+    	
+    	top1Panel.add(new JLabel("WAHH"), BorderLayout.NORTH);
+    	top1Panel.add(new JTextField(10));
+    	
+    	top2Panel.add(new JLabel("YOWZA"));
+    	top2Panel.add(new JTextField(10));
+    	
+    	//Insert top piece
+    	topMainPanel.add(top1Panel);
+    	topMainPanel.add(top2Panel);
+    	topMainPanel.add(nextPlayerButton);
+    	topMainPanel.add(accusationButton);
+    	MainPanel.add(topMainPanel, BorderLayout.NORTH);
+    	
+    	//Create the bottom panel in the big panel
+    	JPanel bottomMainPanel = new JPanel(new GridLayout(1,2));
+    	JPanel bottomLeftPanel = new JPanel(new GridLayout(2,1));
+    	JPanel bottomRightPanel = new JPanel(new GridLayout(2,1));
+    	
+    	//Add to bottomPanel
+    	bottomLeftPanel.add(new JLabel("YOINKS"), BorderLayout.NORTH);
+    	bottomLeftPanel.add(new JTextField(10));
+    	
+    	bottomRightPanel.add(new JLabel("Yowza"), BorderLayout.NORTH);
+    	bottomRightPanel.add(new JTextField(10));
+    	
+    	bottomMainPanel.add(bottomLeftPanel);
+    	bottomMainPanel.add(bottomRightPanel);
+
+    	
+    	//Insert Bottom Piece
+    	MainPanel.add(bottomMainPanel, BorderLayout.SOUTH);
+    	
+    	add(MainPanel);
+    }
+    
+    
+    
 	public void setTurn(Player player, int turn) {
 		//stub
 	}
