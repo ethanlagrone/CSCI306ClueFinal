e6f20c5715ef036ef767243376274d2cd88d74b3
ethanlagrone <ethan_lagrone@mines.edu>
2025-11-14 11:59:06 -0700

    Created skeleton for CardsGui

--- src/clueGame/ClueCardsGUI.java
+++ src/clueGame/ClueCardsGUI.java
@@ -1,5 +1,14 @@
 package clueGame;
 
-public class ClueCardsGUI {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+public class ClueCardsGUI extends JPanel{
 
 }
