054102a10a5d351d2cb8b026ba728043a7367e6c
Morgan Smith <msmith120304@gmail.com>
2025-11-24 13:10:32 -0700

    moved main

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -651,28 +651,4 @@ public class Board extends JPanel {
 			g.drawOval(xOffset * cellWidth, yOffset * height, cellWidth, cellHeight);
 		}
     }
-	
-	public static void main(String[] args) {
-		Board board = Board.getInstance();
-		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
-	    board.initialize();
-	    board.prepareCards();
-
-
-	    ClueCardsGUI clueCards = new ClueCardsGUI(board.getPlayers());
-	    GameControlPanel gameControlPanel = new GameControlPanel();
-	    JFrame frame = new JFrame("Clue Game Board");
-
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setSize(900, 900);
-	    frame.add(board, BorderLayout.CENTER);
-	    frame.add(clueCards, BorderLayout.EAST);	    
-	    frame.add(gameControlPanel, BorderLayout.SOUTH);
-	    
-	    frame.setVisible(true);
-	    
-	    
-	    
-	    
-	}
 }
--- src/clueGame/ClueGame.java
+++ src/clueGame/ClueGame.java
@@ -2,9 +2,29 @@ package clueGame;
 
 import javax.swing.JFrame;
 
+import clueGame.Board;
+import clueGame.ClueCardsGUI;
+import clueGame.GameControlPanel;
+
 public class ClueGame extends JFrame {
     
     public static void main(String[] args) {
-        
+        Board board = Board.getInstance();
+		board.setConfigFiles("ClueLayout.csv", "ClueSetup.txt");
+	    board.initialize();
+	    board.prepareCards();
+
+
+	    ClueCardsGUI clueCards = new ClueCardsGUI(board.getPlayers());
+	    GameControlPanel gameControlPanel = new GameControlPanel();
+	    JFrame frame = new JFrame("Clue Game Board");
+
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setSize(900, 900);
+	    frame.add(board, BorderLayout.CENTER);
+	    frame.add(clueCards, BorderLayout.EAST);	    
+	    frame.add(gameControlPanel, BorderLayout.SOUTH);
+	    
+	    frame.setVisible(true);
     }
 }
