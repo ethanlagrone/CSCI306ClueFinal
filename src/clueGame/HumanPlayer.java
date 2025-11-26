bf40b784c61b29d3fd98d8d345d36830645e18f2
Morgan Smith <msmith120304@gmail.com>
2025-11-26 02:06:18 -0500

    extra commit (didn't save)

--- src/clueGame/HumanPlayer.java
+++ src/clueGame/HumanPlayer.java
@@ -8,12 +8,12 @@ public class HumanPlayer extends Player{
 		super(name, color, row, column);
 	}
 	
-	public BoardCell selectTarget(Set<BoardCell> targets) {
-		/* 
+	/* 
 		this is probably awful practice but i didn't want to deal with typecasting in
 		some of my logic so this is just here now
-		*/
-		System.out.println("If you're seeing this, Morgan messed something up BAD at 2am while already home for break");
+	*/
+	public BoardCell selectTarget(Set<BoardCell> targets) {
+		System.out.println("If you're seeing this, Morgan messed something up BAD at 2am while already home for break.");
 		return null;
 	}
 	
