70f95156ad3bd9522b2389d5cb14bbbb557f346f
Morgan Smith <msmith120304@gmail.com>
2025-12-03 13:42:28 -0700

    epic order of operations fail

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -676,7 +676,7 @@ public class Board extends JPanel implements MouseListener{
 
 	public void progressTurn() {
 		if (currentPlayer == null) {
-			currentPlayer = players.get((int)(Math.random() * players.size() - 1) + 1);
+			currentPlayer = players.get((int)(Math.random() * (players.size() - 1)) + 1);
 		}
 		else if (players.getLast().getName().equals(currentPlayer.getName())) {
 			currentPlayer.setTurnDone(false);
